a53b5f5cc2b166cecac3e3d6baa69c3daa8ca9b6
Swtsngh <40569246+Swtsngh@users.noreply.github.com>
2022-07-17 20:51:56 +0530

    Update app.py

--- flaskapp-docker/flaskapp/app.py
+++ flaskapp-docker/flaskapp/app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template, url_for, redirect
-from flask_sock import Sock
 app = Flask(__name__)
 
-sock = Sock(app)
-@sock.route('/')
+@app.route("/")
 def index():
     return render_template("home.html")
 
-@sock.route("/home")
+@app.route("/home")
 def home():
     return redirect(url_for('login'))
 
-@sock.route("/login")
+@app.route("/login")
 def login():
     return render_template("login.html")
